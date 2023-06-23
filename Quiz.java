8ebe2c8ac18021312d9b12b22ad0168167dbb7e5
Stephen M <s.monk@imprintplus.com>
2023-06-23 14:44:12 -0700

    Creating the originial file for the week 2 assignments

---
+++ Quiz.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+public class Quiz{
+    
+
+    public static void main(String[] args){
+        String question = "What is the largest mammal? \n";
+        question += "A. Elephant\n";
+        question += "B. Whale Shark\n";
+        question += "C. Blue Whale\n";
+        question += "D. Hippopotamus\n";
+        question += "E. Polar Bear\n";
+
+        
+        while(true){
+            String answer = JOptionPane.showInputDialog(question);
+            answer = answer.toUpperCase();
+            if (answer.equals("C")){
+                JOptionPane.showMessageDialog(null, "Correct!");
+                break;
+            }
+            else if(answer.equals("A") || answer.equals("B") || answer.equals("D") || answer.equals("E")){
+                JOptionPane.showMessageDialog(null, "Incorrect! Please try again.");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Invalid Input! Please use only A, B, C, D, or E.");
+            }
+        } 
+        
+
+    }
+
+
+
+}
